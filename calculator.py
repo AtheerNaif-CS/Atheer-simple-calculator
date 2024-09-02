82086420c606d43348b2f65831a01e486cdeb9d6
Atheer Naif <atheer9129@gmail.com>
2024-09-02 09:42:43 +0300

    Solution

--- calculator.py
+++ calculator.py
@@ -1,26 +1,49 @@
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    pass
-
-
-def divide(x, y):
-    pass
-
-
-def multiply(x, y):
-    pass
-
-
+def add(x,z):
+    return x+z
+def subtract(x,z):
+    return x-z
+def multiply():
+    return x*z 
+def devide(x,z):
+    if z==0 :
+        return "Invalid value for denominator, cant't divide by 0!"
+    return x/z
 def square(x):
-    pass
+    return x**2
+def power(x,z):
+    return x**z 
+def square_root(x):
+    return X**0.5 
 
 
-def power(x, y):
-    pass
+def test_add():
+    assert add(5,4) == 9
+    assert add(-3,6) == 3
+
+def test_subtract():
+    assert subtract(5,4) == 1
+    assert subtract(-3,6) == -9
+
+def test_multiply():
+    assert multiply(5,4) == 20
+    assert multiply(-3,6) == -18
+
+def test_devide():
+    assert devide(10,5) == 2
+    assert devide(8,2) == 4
+
+def test_square():
+    assert square(3) == 9
+    assert square(4) == 16
+
+def test_power():
+    assert power(2,3) == 8
+    assert power(3,2) == 9
+
+def test_square_root():
+    assert square_root(9) == 3
+    assert add(81) == 9
 
 
-def sqrt(x):
-    pass
+print(add(7,6))
+
